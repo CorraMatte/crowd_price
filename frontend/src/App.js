01b783620285d38c68bc750e7656120573efdfa1
Matteo Corradini <corra.matteo@gmail.com>
2021-02-12 00:12:42 +0100

    Update main of frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import {Header} from "./components/Header"
+import {BaseRouter} from "./routes"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// Dispatch to each page
+class App extends React.Component {
+
+    render() {
+        return (
+            <Router>
+                <Header {...this.props}/>
+                <BaseRouter {...this.props}/>
+            </Router>
+        );
+    }
 }
 
 export default App;
