1f71cc98a27b5624d6ef0e4c3fba6b79a5b85a43
Matteo Corradini <corra.matteo@gmail.com>
2021-02-13 09:55:20 +0100

    Update routes and endpoints APIT

--- frontend/src/endpoints.js
+++ frontend/src/endpoints.js
@@ -9,6 +9,10 @@ export const REPORTS_NEAREST_API = `${base_url}/reports/nearest`
 export const MOST_REPORTED_PRODUCTS_API = `${base_url}/products/most_reported`
 export const SEARCH_LATEST_API = `${base_url}/search/latest`
 export const SEARCH_STARRED_API = `${base_url}/search/starred`
+export const STORES_API = `${base_url}/stores`
+export const REPORTS_STORE_API = `${base_url}/reports/store/`
+export const PRODUCT_API = `${base_url}/product`
+export const CONSUMER_API = `${base_url}/consumer`
 export const CATEGORIES_API = `${base_url}/categories`
 
 // export const STORES_API = `${base_url}/stores`
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,11 +1,16 @@
 import React from "react";
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from "./components/Home";
+import Stores from "./components/Stores";
+import Profile from "./components/Profile";
+import Login from './components/Login';
+import {isLoggedIn, logOut} from "./auth";
 import LoginRequiredRoute from './custom_routes/LoginRequiredRoute'
+import {Button} from "react-bootstrap";
 
 
 /* Import pages */
-// import Login from './pages/Login';
+//
 //
 // import Overview from './pages/Overview';
 // import AddBarrel from './pages/AddBarrel';
@@ -22,6 +27,10 @@ export class BaseRouter extends React.Component {
         return (
             <Router>
                 <Route path="/home/" component={Home} />
+                <Route path="/stores/" component={Stores} />
+                {/*<LoginRequiredRoute path='/upload/' component={Upload}/>*/}
+                <LoginRequiredRoute path='/profile/' component={Profile}/>
+
 
                 {/*
                 <Route path="/login/" component={Login} />
