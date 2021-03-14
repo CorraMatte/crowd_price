e010154d539242de432190415a01ad4ff0eb4b17
Matteo Corradini <corra.matteo@gmail.com>
2021-03-14 09:48:37 +0100

    Add pagination to store

--- frontend/src/pages/MainStore.js
+++ frontend/src/pages/MainStore.js
@@ -5,8 +5,9 @@ import {DetailGroupReport} from "../components/report/DetailGroupReport"
 import {isLoggedIn} from "../auth";
 import HeaderLogged from "../components/utils/HeaderLogged";
 import {HeaderUnLogged} from "../components/utils/HeaderUnLogged";
-import {Card, Col, Container, Row} from "react-bootstrap";
+import {Button, Card, Col, Container, Row} from "react-bootstrap";
 import DynMap from "../components/map/DynMap";
+import {_update_reports} from "../components/utils/utils";
 
 
 class MainStore extends React.Component {
@@ -15,8 +16,14 @@ class MainStore extends React.Component {
         this.state = {
             store: null,
             reports: [],
-            total_reports: 0
+            next_reports_url: '',
+            prev_reports_url: '',
         };
+    }
+
+    update_reports = (e) => {
+        e.preventDefault();
+        _update_reports(this, e);
     }
 
     componentDidMount() {
@@ -32,20 +39,20 @@ class MainStore extends React.Component {
         axios.get(`${REPORTS_STORE_API}/${id}`).then(
             res => {
                 this.setState({
-                    total_reports: res.data.count,
-                    reports: res.data.results.features
+                    reports: res.data,
+                    next_reports_url: res.data.next,
+                    prev_reports_url: res.data.previous,
                 })
         });
     }
 
     render () {
-        if (!this.state.store || !this.state.reports) {
+        if (!this.state.store) {
             return (<Container></Container>)
         }
 
         const coords = this.state.store.geometry.coordinates.slice();
         const store = this.state.store.properties;
-        const reports = this.state.reports;
 
         const popup = {
             longitude: coords[0],
@@ -63,7 +70,7 @@ class MainStore extends React.Component {
                                 <Card.Img variant={"top"} src={store.picture} className={'mw-100'}/>
                                 <Card.Header><h4>{store.name}</h4></Card.Header>
                                 <Card.Body>
-                                    There are {this.state.total_reports} reports in this store
+                                    There are {this.state.reports.count} reports in this store
                                 </Card.Body>
                             </Card>
                         </Col>
@@ -79,7 +86,13 @@ class MainStore extends React.Component {
                             <h3>Reports in this store</h3>
                         </Card.Header>
                         <Card.Body>
-                            <DetailGroupReport reports={reports} />
+                            <Button id={"previous"} onClick={this.update_reports} className={"float-left"}
+                                    disabled={!this.state.prev_reports_url}>previous</Button>
+                            <Button id={"next"} onClick={this.update_reports} className={"float-right"}
+                                    disabled={!this.state.next_reports_url}>next</Button>
+                        </Card.Body>
+                        <Card.Body>
+                            <DetailGroupReport reports={this.state.reports} />
                         </Card.Body>
                     </Card>
                 </Container>
