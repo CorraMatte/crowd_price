e5515de97d7a28d2b7ad164635a06cdf0489eb78
Matteo Corradini <corra.matteo@gmail.com>
2021-03-27 13:18:50 +0100

    Add Admin customization for products

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,40 @@
 from django.contrib import admin
 from products.models import *
 
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        'name'
+    ]
+    search_fields = [
+        'name'
+    ]
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'name'
+    ]
+    search_fields = [
+        'name'
+    ]
+
+
+class StoreAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Name', {'fields': ['name']}),
+        ('Store on the map', {'fields': ['pnt']}),
+        ('Picture', {'fields': ['picture']})
+    ]
+    list_display = [
+        'name', 'pnt'
+    ]
+    search_fields = [
+        'name'
+    ]
+
+
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Store)
-admin.site.register(Category)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(Category, CategoryAdmin)
